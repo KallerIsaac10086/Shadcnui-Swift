0fb3783160ec712023840914e8b5502f13ef25e6
Kaller <123815501+KallerIsaac10086@users.noreply.github.com>
2026-07-28 17:21:00 +0800

    add:swift_proj

---
+++ ShadcnSwift/ShadcnSwift/ShadcnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadcnSwiftApp.swift
+//  ShadcnSwift
+//
+//  Created by Kaller Isaac on 2026/7/28.
+//
+
+import SwiftUI
+
+@main
+struct ShadcnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShadcnSwift/ShadcnSwiftTests/ShadcnSwiftTests.swift
@@ -0,0 +1,18 @@
+//
+//  ShadcnSwiftTests.swift
+//  ShadcnSwiftTests
+//
+//  Created by Kaller Isaac on 2026/7/28.
+//
+
+import Testing
+
+struct ShadcnSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
